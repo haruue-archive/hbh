e8c5e033d3cccb1d6febab74382b10a576b480e1
Haruue Icymoon <haruue@caoyue.com.cn>
2018-06-01 10:15:14 +0800

    feat: checkable radio '我第一次索票'... in page merry
    Signed-off-by: Haruue Icymoon <haruue@caoyue.com.cn>

--- pages/marry/marry.js
+++ pages/marry/marry.js
@@ -1,7 +1,8 @@
 Page({
   data:{
     flag:'1',
-    way:'请选择获知渠道'
+    way:'请选择获知渠道',
+    ticketType:'1'
   },
   selectWay:function(){
      this.setData({flag:'0'});
@@ -17,5 +18,9 @@ Page({
     var ticket = e.detail.value;
      ticket.way = this.data.way;
      wx.setStorageSync('ticket', ticket);
+  },
+  onTicketTypeSelect:function(e) {
+    console.log(e);
+    this.setData({ticketType: e.target.dataset.item})
   }
 })
